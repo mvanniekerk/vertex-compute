c9987cc0e3be1b0276283aa110e1f59e6ba42a93
Matthijs van Niekerk <matthijsvanniekerk@gmail.com>
2021-10-02 12:09:49 +0200

    support reloading of state on the frontend

--- src/main/java/com/mvanniekerk/akka/compute/control/SystemDescription.java
+++ src/main/java/com/mvanniekerk/akka/compute/control/SystemDescription.java
@@ -5,5 +5,5 @@ import com.mvanniekerk.akka.compute.vertex.VertexDescription;
 import java.util.List;
 
 public record SystemDescription(List<VertexDescription> vertices, List<Edge> edges) {
-    public record Edge(String id, String fromId, String toId) {}
+    public record Edge(String id, String from, String to) {}
 }
